eb8473c2eff4607558bbfddb855591500e37de20
Michael <chimzeboi07@gmail.com>
2021-04-30 17:52:30 -0700

    testing local storage darkmode

--- index.js
+++ index.js
@@ -17,11 +17,38 @@ toggle.addEventListener('click', () => {
 });
 
 
-checkbox.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+checkbox.addEventListener('click', () => {
+
+    darkMode = localStorage.getItem('darkMode');
+
+    if(darkMode !=="enabled"){
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 });
 
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
 });
+
+let darkMode = localStorage.getItem('darkMode');
+
+const enableDarkMode = () => {
+
+    document.body.classList.add('dark');
+
+    localStorage.setItem("darkMode", "enabled");
+}
+
+const disableDarkMode = () => {
+    localStorage.setItem("darkMode", null);
+
+    document.body.classList.remove('dark');
+
+}
+
+if(darkMode === 'enabled'){
+    enableDarkMode(); 
+}
